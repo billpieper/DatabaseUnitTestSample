1201ab8680707d155352c1ec4d5a24ed52624231
Bill Pieper <bill.pieper@intelligentinsites.com>
2016-09-22 19:10:12 -0500

    sample project

---
+++ DatabaseUnitTests/Test_Sysadmin_Role.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseUnitTestsProject
+{
+    [TestClass()]
+    public class Test_Sysadmin_Role : SqlDatabaseTestClass
+    {
+
+        public Test_Sysadmin_Role()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        [TestMethod()]
+        public void Test_Sysadmin_Role_Exists()
+        {
+            SqlDatabaseTestActions testActions = this.Test_Sysadmin_Role_ExistsData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            // Execute the test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+            SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            // Execute the post-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+            SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+        }
+
+
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction Test_Sysadmin_Role_Exists_TestAction;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Test_Sysadmin_Role));
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.RowCountCondition rowCountCondition1;
+            this.Test_Sysadmin_Role_ExistsData = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestActions();
+            Test_Sysadmin_Role_Exists_TestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            rowCountCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.RowCountCondition();
+            // 
+            // Test_Sysadmin_Role_Exists_TestAction
+            // 
+            Test_Sysadmin_Role_Exists_TestAction.Conditions.Add(rowCountCondition1);
+            resources.ApplyResources(Test_Sysadmin_Role_Exists_TestAction, "Test_Sysadmin_Role_Exists_TestAction");
+            // 
+            // Test_Sysadmin_Role_ExistsData
+            // 
+            this.Test_Sysadmin_Role_ExistsData.PosttestAction = null;
+            this.Test_Sysadmin_Role_ExistsData.PretestAction = null;
+            this.Test_Sysadmin_Role_ExistsData.TestAction = Test_Sysadmin_Role_Exists_TestAction;
+            // 
+            // rowCountCondition1
+            // 
+            rowCountCondition1.Enabled = true;
+            rowCountCondition1.Name = "rowCountCondition1";
+            rowCountCondition1.ResultSet = 1;
+            rowCountCondition1.RowCount = 1;
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+        private SqlDatabaseTestActions Test_Sysadmin_Role_ExistsData;
+    }
+}
